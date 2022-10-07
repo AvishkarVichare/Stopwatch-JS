450fe279e3e98010d6340626e68b7b60e9583300
Avishkar Vichare <avie9500@gmail.com>
2022-10-07 18:22:28 +0530

    added more features like progress bar and footer

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const h = document.querySelector('#h');
 const m = document.querySelector('#m');
 const s = document.querySelector('#s');
 const cs = document.querySelector('#cs');
+const rotator = document.querySelector('#rotator');
 
 const start = document.querySelector('#start');
 const stop = document.querySelector('#stop');
@@ -12,6 +13,7 @@ let secs = 0;
 let hrs = 0;
 let mins = 0;
 let id = null;
+let degToRoate = 0;
 
 function startWatch(){
     if(mins>=60){
@@ -26,12 +28,15 @@ function startWatch(){
         m.innerText = mins<10?`0${mins}`:mins;
         secs = 00;
         
+        
       }
 
       if(centSecs>=100){
         secs ++;
         s.innerText = secs<10?(`0${secs}`):secs;
         centSecs = 0;
+        degToRoate = (36*secs)/6;
+        rotator.style.transform = `rotate(${degToRoate}deg)`
       }
       
       centSecs++;
@@ -43,12 +48,20 @@ function startWatch(){
 
 
 start.addEventListener('click',()=>{
+    if(id){
+        clearInterval(id);
+    }
     id = setInterval(startWatch, 10);
 
 })
 
 stop.addEventListener('click',()=>{
-    clearInterval(id)
+    clearInterval(id);
+    h.innerText = hrs<10?`0${hrs}`:hrs;
+    m.innerText = mins<10?`0${mins}`:mins;
+    s.innerText = secs<10?(`0${secs}`):secs;
+    cs.innerText = centSecs<10?`0${centSecs}`:centSecs;
+
 })
 
 reset.addEventListener('click',()=>{
@@ -61,4 +74,7 @@ reset.addEventListener('click',()=>{
     m.innerText = '00';
     s.innerText = '00';
     cs.innerText = '00';
+    rotator.style.transform = `rotate(0deg)`;
+
 })
+
